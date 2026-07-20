72eca7d691c821e468a21c49f71afd0cb1dfeee3
samuelefollin <samuelepython@gmail.com>
2026-07-20 08:45:00 +0000

    modifiche esercizio 3

--- Esercizio3.py
+++ Esercizio3.py
@@ -1,3 +1,11 @@
+#File: Esercizio2.py
+#
+#Autore: Samuele Follin
+#
+#Data: 20/07/2026
+#
+#Descrizione: Svolgimento dell'Esercizio 2
+
 rubrica = {
   'Paolino Paperino': {'giorno': 9,
                       'mese': 'giugno',
@@ -15,19 +23,22 @@ rubrica = {
 'Madoka Ayukawa': {'giorno': 25, 'mese': 'maggio', 'anno': 1969, 'età': 57, 'sesso': 'F', 'mail': 'madoka_sax@asahi_net.jp'}
 }
 
-#-----------------------------------------------
+#----- PUNTO 1 -----
 
 def punto_uno():
-    for personaggio in rubrica.keys():
-        print(str(personaggio), end = ', ')                               #stampo prima le chiavi principali, con end = ', ' la stampa termina con virgola e spazio
+    '''stampa a schermo delle stringhe contenenti i dati dei personaggi'''
+    for personaggio in rubrica.keys():                                 #loop iterato per ogni chiave del dizionario
+        print(personaggio, end = ', ')                                 #stampo prima le chiavi principali, con end = ', ' la stampa termina con virgola e spazio
         for dato in rubrica[personaggio]:                               
-         print(str(dato), end = ': ')                                  #stampo i valori delle chiavi personaggi, ossia le chiavi delle liste annidate
-         print(str(rubrica[personaggio][dato]), end = ', ')            #stampo i valori delle chiavi delle liste annidate
+         print(str(dato), end = ': ')                                  #i valori dei personaggi sono altri dizionari, stampo le chiavi di questi dizionari, ossia le chiavi dei dizionari annidati
+         print(str(rubrica[personaggio][dato]), end = ', ')            #stampo i valori dei dizionari annidati
 
 punto_uno()
-#------------------------------------------------
+
+#----- PUNTO 2 -----
 
 def punto_due():
+    '''costruisce la lista crescente delle età'''
     lista_età = []
     for personaggio in rubrica:
         lista_età.append(rubrica[personaggio]['età'])                 #inserisco nella lista il valore di 'età', che è valore del personaggio
@@ -35,24 +46,28 @@ def punto_due():
     print(lista_età)
     for anni in lista_età:                                            #prendo in rassegna la lista appena creata
         for personaggio in rubrica:                                   #prendo in rassegna i 4 personaggi del dizionario
-            if rubrica[personaggio]['età'] == anni:                   #se il valore dell'età del personaggio è uguale al valore nella lista, stampo il nome del personaggio
+            if rubrica[personaggio]['età'] == anni:                   #controllo per ogni personaggio se la sua età corrisponde, in caso affermativo lo stampo
              print(personaggio)
 
 punto_due()
-#-------------------------------------------------
+
+#----- PUNTO 3 -----
 
 def punto_tre():
+    '''costruisce la lista decrescente delle età'''
     lista_età = []
     for personaggio in rubrica:
         lista_età.append(rubrica[personaggio]['età'])
-    lista_età_decrescente = lista_età[::-1]                           #slicing di passo -1 e di inizio-fine nulli
-    print(lista_età_decrescente)
+    lista_età_decrescente = lista_età[::-1]                          #è uno slicing, infatti [inizio, fine, passo] in questo caso è composto solo dal passo, che essendo -1 va all'indietro
+    print(lista_età_decrescente)                                     #quindi, l'ordine della lista_età è invertito
 
 punto_tre()
-#-------------------------------------------------
+
+#----- PUNTO 4 -----
 
 def punto_quattro():
-    for personaggio in rubrica:                                      #prepariamo una formattazione f-strings, assegnamo tutte le variabili ai valori correlati
+    '''scrive una frase personalizzata per ogni personaggio'''
+    for personaggio in rubrica:                                      #prepariamo una formattazione f-string, assegnamo tutte le variabili ai valori correlati
         nome = personaggio
         giorno = rubrica[personaggio]['giorno']
         mese = rubrica[personaggio]['mese']
@@ -62,29 +77,31 @@ def punto_quattro():
         if rubrica[personaggio]['sesso'] == 'M':                     #dividiamo i due casi per il sesso
             sesso = 'o'
         else: sesso = 'a'
+
         frase = f'Car{sesso} {nome}, sei nat{sesso} il {giorno} di {mese} del {anno} e quindi a breve compirai {età} anni. Ti manderemo gli auguri per {mail}'
         print(frase)
 
 punto_quattro()
-#---------------------------------------------------
 
-'''import sys                                                    #necessario per utilizzare argv
+#----- PUNTO 5 -----
+
+import sys                                                    #necessario per utilizzare argv
 
 for personaggio in rubrica:
     chiave = sys.argv[1]                                      #la chiave deve essere uguale al secondo valore della lista di argomenti (il primo è il nome del programma)
-    print(rubrica[personaggio][chiave])'''
+    print(rubrica[personaggio][chiave])
 
-#----------------------------------------------------
+#----- PUNTO 6 -----
 
 import argparse
 
-'''parser = argparse.ArgumentParser()                       #creo una variabile e le assegno un oggetto ArgumentParser
+parser = argparse.ArgumentParser()                       #creo una variabile e le assegno un oggetto ArgumentParser
 parser.add_argument('-nome')                             #aggiungo il parametro da inserire al momento dell'esecuzione del programma
-args = parser.parse_args()                               #così avviene l lettura dell'argomento
+args = parser.parse_args()                               #così avviene la lettura dell'argomento
 
-personaggio = args.nome                                 #la variabie personaggio diventa il nome indicato all'esecuzione del programma
+personaggio = args.nome                                  #la variabie personaggio diventa il nome indicato all'esecuzione del programma
 
-nome = personaggio                                      #senza questo il nome all'inizio della frase è quello dell'ultimo personaggio della rubrica
+nome = personaggio                                       #senza questo il nome all'inizio della frase è quello dell'ultimo personaggio della rubrica
 giorno = rubrica[personaggio]['giorno']
 mese = rubrica[personaggio]['mese']
 anno = rubrica[personaggio]['anno']
@@ -94,10 +111,11 @@ if rubrica[personaggio]['sesso'] == 'M':
     sesso = 'o'
 else: sesso = 'a'
 frase = f'Car{sesso} {nome}, sei nat{sesso} il {giorno} di {mese} del {anno} e quindi a breve compirai {età} anni. Ti manderemo gli auguri per {mail}'
-print(frase)'''
+print(frase)
 
-#----------------------------------------------------
-def punto_cinque(dato):                                               #trasformo i punti precedenti in funzioni, per il 5° punto creo una funzione a parte con parametro 
+#----- PUNTO 7 -----
+
+def punto_cinque(dato):                                               #creo la funzione con parametro per il punto 5
     for persona in rubrica:
         print(rubrica[persona][dato])
 
@@ -108,13 +126,13 @@ parser.add_argument('-punto_tre', action = 'store_true')
 parser.add_argument('-punto_quattro', action = 'store_true')
 parser.add_argument('-punto_cinque')                                 #nel caso del quinto punto è necessario un valore di fianco a -punto_cinque nella chiamata
 args = parser.parse_args()
-if args.punto_uno:
+if args.punto_uno:                                                   #verrà eseguito l'if del punto indicato
     punto_uno()
-if args.punto_due:
+elif args.punto_due:
     punto_due()
-if args.punto_tre:
+elif args.punto_tre:
     punto_tre()
-if args.punto_quattro:
+elif args.punto_quattro:
     punto_quattro()
-if args.punto_cinque:
+elif args.punto_cinque:
     punto_cinque(args.punto_cinque)
