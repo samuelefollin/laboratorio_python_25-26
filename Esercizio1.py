ad3f4ab621bde7f281616a561393b5fb58eacf9e
samuelefollin <samuelepython@gmail.com>
2026-07-23 13:14:10 +0000

    ultime modifiche esercizio 1

--- Esercizio1.py
+++ Esercizio1.py
@@ -9,14 +9,15 @@
 
 print('Quanti numeri vuoi testare?')
 tentativi = int(input())
-if int(tentativi) <= 0:
+if tentativi <= 0:
     print('Peggio per te...')
 else:
-    print('Ottimo, inizia pure.')
+    print('Ottimo. Inizia pure, inserisci il primo numero.')
 
 #----- PUNTO 1 -----
 
-n = int(input())                                         #l'input deve essere un nmero intero
+if tentativi > 0:                                            #se i tentativi sono <= 0, l'input non viene chiesto e l'esecuzione del codice si è giò fermata
+    n = int(input())                                         #l'input deve essere un nmero intero
 
 def is_pari(n):                                              #il parametro della funzione è il numero inserito in input
     '''controlla se il numero inserito è un numero pari'''
@@ -48,7 +49,8 @@ def genera_lista(n):
 
 #----- PUNTO 4 -----
 
-lista = genera_lista(n)                              #associo alla variabile lista la lista generata dal punto precedente
+if tentativi > 0:
+    lista = genera_lista(n)                              #associo alla variabile lista la lista generata dal punto precedente
 
 def analizza_sequenza(lista):
     '''definisce il massimo e la lunghezza della lista e la somma di tutti i suoi elementi'''
@@ -61,26 +63,33 @@ def ricerca(lista):
     for valore in lista:
         if valore % 5 == 0:                                       #se il resto della divisione per 5 è 0 (cioè il numero è multpilo di 5)
             print(valore)
-    if all(valore % 5 != 0 for valore in lista):                  #all() restituisce True se tutti i valori di un iterabile sono True, False se almeno uno è False
-        print('Non sono presenti numeri divisibili per 5')        #l'if va messo fuori dal for, se no stampa il messaggio per ogni numero non multiplo di 5
+    
+    no_multipli = True
+    for valore in lista:
+        if valore % 5 == 0:
+            no_multipli = False
+            break
+    if no_multipli is True:
+        print("Non sono presenti numeri divisibili per 5")
 
 #----- PUNTO 6 -----
 
 lunghezza_massima = 0                                                    #inizializzo: do un valore di partenza alla lunghezza massima
 numero_migliore = 0                                                      #inizializzo: do un valore di partenza al numero migliore
 
-for tentativo in range(tentativi):                                       #le funzioni sono ripetute il numero di volte indicato dall'utente                                                    
-    print(is_pari(n))                                                    #chiamo is_pari con il print, così il return viene stampato a schermo
-    n = is_positivo(n)                                                   #chiamo la funzione is_positivo e salvo il valore generato in una variabile
-    lista = genera_lista(n)                                              #in questo modo genera sempre nuove liste, se no studia sempre e solo la prima
-    print(genera_lista(n))                                               #chiamo genera_lista e stampo a schermo la lista generata con return
-    print(analizza_sequenza(lista))                                      #chiamo analizza_sequenza e stampo a schermo i valori (raggruppati in una tupla) del return
-    ricerca(lista)                                                       #chiamo ricerca senza print perché è già all'interno della funzione
-    if len(lista) > lunghezza_massima:                                   #cerco la lista più lunga, con questo if aggiorno la lunghezza massima e il numero migliore a essa associato
-        lunghezza_massima = len(lista)                                   
-        numero_migliore = n
-    if tentativo < tentativi - 1:                                        #senza questo if chiederebbe un input per poi ignorarlo e stampare il messaggio dei tentativi esauriti
-        print('Prossimo numero?')
-        n = int(input())
-    
-    print('Hai finito i tentativi. Il numero iniziale che ha generato la sequenza più lunga è il:', numero_migliore)
+if tentativi > 0:
+    for tentativo in range(tentativi):                                       #le funzioni sono ripetute il numero di volte indicato dall'utente                                                    
+        print(is_pari(n))                                                    #chiamo is_pari con il print, così il return viene stampato a schermo
+        n = is_positivo(n)                                                   #chiamo la funzione is_positivo e salvo il valore generato in una variabile
+        lista = genera_lista(n)                                              #in questo modo genera sempre nuove liste, se no studia sempre e solo la prima
+        print(lista)                                                         #stampo la lista
+        print(analizza_sequenza(lista))                                      #chiamo analizza_sequenza e stampo a schermo i valori (raggruppati in una tupla) del return
+        ricerca(lista)                                                       #chiamo ricerca senza print perché è già all'interno della funzione
+        if len(lista) > lunghezza_massima:                                   #cerco la lista più lunga, con questo if aggiorno la lunghezza massima e il numero migliore a essa associato
+            lunghezza_massima = len(lista)                                   
+            numero_migliore = n
+        if tentativo < tentativi - 1:                                        #senza questo if chiederebbe un input per poi ignorarlo e stampare il messaggio dei tentativi esauriti
+            print('Prossimo numero?')
+            n = int(input())
+        
+        print('Hai finito i tentativi. Il numero iniziale che ha generato la sequenza più lunga è il:', numero_migliore)
--- modifiche.py
+++ modifiche.py
@@ -1,52 +1,86 @@
-print('Ciao! Ti va di fare un paio di tabelline?')
-while True:                                                                                      #ciclo infinito, condizione sempre vera... si esce solo grazie al break
-    try:                                                                                         #uso try-except, così gestisco i caratteri e i numeri che sono decimali
-        numero = int(input('Dimmi un numero intero! '))                                          #senza int() l'input è considerato stringa e i risultati delle tabelline sono sbagliati
-        break                                                                                    #permette di uscire dal ciclo, l'unico modo è passare per break scegliendo un numero intero
-    except ValueError:
-        print('Ehi, scegli un numero intero! Non digitare lettere, caratteri speciali o numeri decimali.')                        #continuo finché non viene digitato un numero intero
+#File: Esercizio1.py
+#
+#Autore: Samuele Follin
+#
+#Data: 18/07/2026
+#
+#Descrizione: Svolgimento dell'Esercizio 1
 
-print(f'Ok... hai scelto il numero {numero}, eh? Vediamo come te la cavi, dovrai indovinare tutti i risultati! Quanto fa {numero} x 0?')
 
-lista_corrette = []                                                                                        #questa lista serve a tenere il conto delle soluzioni corrette
-def tabelline(numero):
-    for i in range(10):                                                                                    #passo in rassegna tutti i numeri da 0 a 9
-        while True:                                                                                        #anche qui devo assicurarmi di ricevere solo interi, stesso while di prima con try-except
-            try:
-                risposta = int(input())                                                                    #anche qui ho bisogno che l'input sia di tipo int, non str
-                break
-            except ValueError:
-                print('Il risultato della moltiplicazione di due interi è un intero, suvvia!')
-        if risposta == numero * i:                                                                          #creo due diverse frasi in base alla risposta data
-            lista_corrette.append(risposta)
-            yield f'Corretto! Era {numero * i}. E ora, quanto fa {numero} x {i+1}?'
-        else:
-            yield f'Hai sbagliato! Il risultato corretto era {numero * i}. Dai, prova a calcolare {numero} x {i+1}'
-
- 
-g = tabelline(numero)                                               #creo così l'oggetto generatore
-
-for i in range(10):
-    print(next(g))                                              #grazie allo yield posso andare avanti con gli indici del range
-
-while True:                                                           #per il risultato di n x 10, stesso try-except di prima
-    try:
-        risposta = int(input())                                       #anche qui ho bisogno che l'input sia intero
-        break
-    except ValueError:
-        print('Il risultato della moltiplicazione di due interi è un intero, suvvia!')
-if risposta == numero * 10:                                                #per il 10 devo creare un if a parte, se lo metto dentro la funzione dovrei fare range(11) e mi chiederebbe n x 11 senza ottenere risposta
-    lista_corrette.append(risposta)
-    print('Corretto!\n')     
+print('Quanti numeri vuoi testare?')
+tentativi = int(input())
+if int(tentativi) <= 0:
+    print('Peggio per te...')
 else:
-    print(f'Hai sbagliato! Il risultato corretto era {numero * 10}\n')  
+    print('Ottimo, inizia pure.')
 
-print(f'Hai totalizzato un punteggio di {len(lista_corrette)}/11')                               #il numero di elementi della lista corrisponde alle risposte corrette, stampo a schermo il punteggio
-if len(lista_corrette) < 4:                                                                      #stampo un messaggio diverso in base al numero di risposte corrette
-    print('Caspita... dovresti fare pratica, puoi solo migliorare!')
-elif 4 <= len(lista_corrette) <= 7:
-    print('Risultato migliorabile, con della pratica puoi correggere tutti i tuoi errori!')
-elif 7 < len(lista_corrette) <= 10:
-    print('Wow, ottimo risultato! Puoi senza dubbio raggiungere la perfezione, se ti eserciti ancora un po')
-elif len(lista_corrette) == 11:
-    print('Le tabelline sono il tuo forte! Ben fatto')
+#----- PUNTO 1 -----
+
+n = int(input())                                         #l'input deve essere un nmero intero
+
+def is_pari(n):                                              #il parametro della funzione è il numero inserito in input
+    '''controlla se il numero inserito è un numero pari'''
+    if int(n) % 2 == 0:                                      #se il resto della divisione è uguale a zero
+        return True
+    else:
+        return False
+    
+#----- PUNTO 2 -----
+
+def is_positivo(n):
+    '''controlla se l'input è un numero positivo e lo restituisce se lo è'''
+    while int(n) <= 0:                                       #uso while perché è un loop con condizione
+        n = int(input())                                     #finché n è negativo o zero il ciclo continua
+    return n        
+
+#----- PUNTO 3 -----
+
+def genera_lista(n):
+    '''applica la congettura di Collatz al numero e genera una lista con tutti i numeri risultanti'''
+    lista = [n]                               #creo una lista che momentaneamente contiene solo l'input
+    while n != 1 and len(lista) <= 100:       #appena una delle due condizioni viene smentita il ciclo si arresta
+        if int(n) % 2 == 0:                   #se il numero è pari viene diviso per due
+            n = int(int(n) / 2)
+        else:                                 #se il numero è dispari viene moltiplicato per 3 e sommato di 1
+            n = int(int(n) * 3 + 1)           
+        lista.append(n)                       #in ogni caso, il numero viene aggiunto alla lista
+    return lista                        
+
+#----- PUNTO 4 -----
+
+lista = genera_lista(n)                              #associo alla variabile lista la lista generata dal punto precedente
+
+def analizza_sequenza(lista):
+    '''definisce il massimo e la lunghezza della lista e la somma di tutti i suoi elementi'''
+    return max(lista), len(lista), sum(lista)        #la funzione restituisce una tupla contenente questi tre valori
+
+#----- PUNTO 5 -----
+
+def ricerca(lista):
+    ''' analizza la lista e stampa a schermo i valori divisibili per 5'''
+    for valore in lista:
+        if valore % 5 == 0:                                       #se il resto della divisione per 5 è 0 (cioè il numero è multpilo di 5)
+            print(valore)
+    if all(valore % 5 != 0 for valore in lista):                  #all() restituisce True se tutti i valori di un iterabile sono True, False se almeno uno è False
+        print('Non sono presenti numeri divisibili per 5')        #l'if va messo fuori dal for, se no stampa il messaggio per ogni numero non multiplo di 5
+
+#----- PUNTO 6 -----
+
+lunghezza_massima = 0                                                    #inizializzo: do un valore di partenza alla lunghezza massima
+numero_migliore = 0                                                      #inizializzo: do un valore di partenza al numero migliore
+
+for tentativo in range(tentativi):                                       #le funzioni sono ripetute il numero di volte indicato dall'utente                                                    
+    print(is_pari(n))                                                    #chiamo is_pari con il print, così il return viene stampato a schermo
+    n = is_positivo(n)                                                   #chiamo la funzione is_positivo e salvo il valore generato in una variabile
+    lista = genera_lista(n)                                              #in questo modo genera sempre nuove liste, se no studia sempre e solo la prima
+    print(genera_lista(n))                                               #chiamo genera_lista e stampo a schermo la lista generata con return
+    print(analizza_sequenza(lista))                                      #chiamo analizza_sequenza e stampo a schermo i valori (raggruppati in una tupla) del return
+    ricerca(lista)                                                       #chiamo ricerca senza print perché è già all'interno della funzione
+    if len(lista) > lunghezza_massima:                                   #cerco la lista più lunga, con questo if aggiorno la lunghezza massima e il numero migliore a essa associato
+        lunghezza_massima = len(lista)                                   
+        numero_migliore = n
+    if tentativo < tentativi - 1:                                        #senza questo if chiederebbe un input per poi ignorarlo e stampare il messaggio dei tentativi esauriti
+        print('Prossimo numero?')
+        n = int(input())
+    
+    print('Hai finito i tentativi. Il numero iniziale che ha generato la sequenza più lunga è il:', numero_migliore)
