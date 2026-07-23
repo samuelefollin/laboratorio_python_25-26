b0760cda323dfe66c1516d8e11d26488e06ac3d3
samuelefollin <samuelepython@gmail.com>
2026-07-23 15:27:37 +0000

    ultime modifiche esercizio 3

--- Esercizio3.py
+++ Esercizio3.py
@@ -1,10 +1,10 @@
-#File: Esercizio2.py
+#File: Esercizio3.py
 #
 #Autore: Samuele Follin
 #
 #Data: 20/07/2026
 #
-#Descrizione: Svolgimento dell'Esercizio 2
+#Descrizione: Svolgimento dell'Esercizio 3
 
 rubrica = {
   'Paolino Paperino': {'giorno': 9,
@@ -32,9 +32,10 @@ def punto_uno():
         for dato in rubrica[personaggio]:                               
          print(str(dato), end = ': ')                                  #i valori dei personaggi sono altri dizionari, stampo le chiavi di questi dizionari, ossia le chiavi dei dizionari annidati
          print(str(rubrica[personaggio][dato]), end = ', ')            #stampo i valori dei dizionari annidati
+         
 
 punto_uno()
-
+print('\n')
 #----- PUNTO 2 -----
 
 def punto_due():
@@ -44,6 +45,7 @@ def punto_due():
         lista_età.append(rubrica[personaggio]['età'])                 #inserisco nella lista il valore di 'età', che è valore del personaggio
     lista_età.sort()                                                  #con .sort() ordino in ordine crescente
     print(lista_età)
+    print('Personaggi in ordine crescente di età:')
     for anni in lista_età:                                            #prendo in rassegna la lista appena creata
         for personaggio in rubrica:                                   #prendo in rassegna i 4 personaggi del dizionario
             if rubrica[personaggio]['età'] == anni:                   #controllo per ogni personaggio se la sua età corrisponde, in caso affermativo lo stampo
@@ -58,8 +60,9 @@ def punto_tre():
     lista_età = []
     for personaggio in rubrica:
         lista_età.append(rubrica[personaggio]['età'])
+    lista_età.sort()
     lista_età_decrescente = lista_età[::-1]                          #è uno slicing, infatti [inizio, fine, passo] in questo caso è composto solo dal passo, che essendo -1 va all'indietro
-    print(lista_età_decrescente)                                     #quindi, l'ordine della lista_età è invertito
+    print(f'Lista delle età in ordine decrescente: {lista_età_decrescente}')
 
 punto_tre()
 
@@ -85,11 +88,11 @@ punto_quattro()
 
 #----- PUNTO 5 -----
 
-import sys                                                    #necessario per utilizzare argv
+import sys                                                                        #necessario per utilizzare argv
 
 for personaggio in rubrica:
-    chiave = sys.argv[1]                                      #la chiave deve essere uguale al secondo valore della lista di argomenti (il primo è il nome del programma)
-    print(rubrica[personaggio][chiave])
+    chiave = sys.argv[1]                                                          #la chiave deve essere uguale al secondo valore della lista di argomenti (il primo è il nome del programma)
+    print(f'{chiave} di {personaggio}: {rubrica[personaggio][chiave]}')           #stampa il dato della chiave per ogni personaggio
 
 #----- PUNTO 6 -----
 
@@ -116,8 +119,8 @@ print(frase)
 #----- PUNTO 7 -----
 
 def punto_cinque(dato):                                               #creo la funzione con parametro per il punto 5
-    for persona in rubrica:
-        print(rubrica[persona][dato])
+    for personaggio in rubrica:
+        print(f'{dato} di {personaggio}: {rubrica[personaggio][dato]}')
 
 parser  = argparse.ArgumentParser()
 parser.add_argument('-punto_uno', action = 'store_true')              #aggiungo i vari parser, action = store_true serve alle funzioni che non necessitano un valore nella chiamata
